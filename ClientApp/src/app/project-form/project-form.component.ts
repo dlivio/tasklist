ee0009ffc1ad40b2976ef0305315628385754b4b
dlivio <48352205+dlivio@users.noreply.github.com>
2021-08-18 19:34:21 +0100

    Added routing to Projects page after creation; UI fixes

--- ClientApp/src/app/project-form/project-form.component.ts
+++ ClientApp/src/app/project-form/project-form.component.ts
@@ -13,10 +13,12 @@ export class ProjectFormComponent implements OnInit {
 
   public client: HttpClient;
   public baseUrl: string;
+  public route: Router;
 
   constructor(http: HttpClient, @Inject('BASE_URL') baseUrl: string, private router: Router) {
     this.client = http;
     this.baseUrl = baseUrl;
+    this.route = router;
   }
 
   model = new Project("-1", "", "", false, "");
@@ -26,20 +28,21 @@ export class ProjectFormComponent implements OnInit {
   onSubmit() {
     this.projectName = (<HTMLInputElement>document.getElementById("projectName")).value;
 
+    // disallow submitting without writting a name
     if (this.projectName == "") {
-      alert("Project name needed.");
+      alert("Project name can't be empty.");
       return;
     }
 
+    // build an object with the project name and creation datetime
     let proj = new ProjectForm(this.projectName, new Date().toISOString());
 
     // change to post
     this.client.post<ProjectForm>(this.baseUrl + 'api/Projects', proj).subscribe(result => {
-      let response = result
-      console.log(response);
+      let response = result;
+      this.route.navigate(['/projects ']);
     }, error => console.error(error));
 
-    console.log(this.projectName);
   }
 
   ngOnInit() {
