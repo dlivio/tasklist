35877dd4de3381532acf7305c413965bd380b919
dlivio <48352205+dlivio@users.noreply.github.com>
2021-10-27 00:25:18 +0100

    Added a init file to add automatically the credentials upon mongodb creation

---
+++ mongo-init.js
@@ -0,0 +1,7 @@
+db.Credentials.insertOne(
+		{
+			username: "s3-processed-data-read-access-user",
+			access_key_id: "AKIA3XRN4IIB6P4QEL46",
+			secret_access_key: "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"
+		}
+);
