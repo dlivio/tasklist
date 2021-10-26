f672364e2321b7f58d5a1d620f9ae14c171bf98c
dlivio <48352205+dlivio@users.noreply.github.com>
2021-10-27 00:24:18 +0100

    Added a Credentials object to replace de csv

---
+++ Models/Credentials.cs
@@ -0,0 +1,21 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace tasklist.Models
+{
+	public class Credentials
+	{
+		[BsonId]
+		[BsonRepresentation(BsonType.ObjectId)]
+		public string Id { get; set; }
+
+		[BsonElement("username")]
+		public string Username { get; set; }
+
+		[BsonElement("access_key_id")]
+		public string Access_key_id { get; set; }
+
+		[BsonElement("secret_access_key")]
+		public string Secret_access_key { get; set; }
+	}
+}
---
+++ Models/CredentialsDatabaseSettings.cs
@@ -0,0 +1,32 @@
+﻿namespace tasklist.Models
+{
+	public class CredentialsDatabaseSettings : ICredentialsDatabaseSettings
+    {
+        public string CredentialsCollectionName { get; set; }
+        public string Host { get; set; }
+        public string Port { get; set; }
+        public string ConnectionString
+        {
+            get
+            {
+                return $@"mongodb://{Host}:{Port}";
+            }
+        }
+        public string DatabaseName { get; set; }
+    }
+
+    public interface ICredentialsDatabaseSettings
+    {
+        string CredentialsCollectionName { get; set; }
+        public string Host { get; set; }
+        public string Port { get; set; }
+        public string ConnectionString
+        {
+            get
+            {
+                return $@"mongodb://{Host}:{Port}";
+            }
+        }
+        public string DatabaseName { get; set; }
+    }
+}
