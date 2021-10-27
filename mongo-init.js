d9665218ca884f88f17ba2bf0c28123276de3e1c
dlivio <48352205+dlivio@users.noreply.github.com>
2021-10-27 14:49:18 +0100

    New keys for Amazon access

--- mongo-init.js
+++ mongo-init.js
@@ -5,7 +5,7 @@ db.createCollection('Credentials');
 db.Credentials.insertOne(
 		{
 			username: "s3-processed-data-read-access-user",
-			access_key_id: "AKIA3XRN4IIB6P4QEL46",
-			secret_access_key: "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"
+			access_key_id: "AKIA3XRN4IIBWSRT3QOA",
+			secret_access_key: "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"
 		}
 );
