aa799eff23f81cbce5116abd5e3f428e3a74492b
dlivio <48352205+dlivio@users.noreply.github.com>
2021-11-21 21:32:20 +0000

    Update mongo-init.js

--- mongo-init.js
+++ mongo-init.js
@@ -18,13 +18,13 @@ db.ActivityMaps.insertMany([
 		activityId: "Activity_0tv9fno",
 		diagramId: "restoration_reevaluation_pickling",
 		diagramOrder: 1,
-		sensorsRelated: ["Electric Sander"] 
+		sensorsRelated: ["Mineral Blast"]
 	},
 	{
 		activityId: "Activity_0xseh9x",
 		diagramId: "restoration_repair_bodywork_part",
 		diagramOrder: 1,
-		sensorsRelated: ["Electric Sander", "Polisher", "Manual Sander"] 
+		sensorsRelated: ["Hammer", "Dolly"] 
 	},
 	{
 		activityId: "Activity_13xga2i",
@@ -36,13 +36,13 @@ db.ActivityMaps.insertMany([
 		activityId: "Activity_1yibt4p",
 		diagramId: "restoration_repair_bodywork_part",
 		diagramOrder: 3,
-		sensorsRelated: ["Electric Sander", "Polisher", "Manual Sander"]
+		sensorsRelated: ["Drill"]
 	},
 	{
 		activityId: "Activity_0i9ezzj",
 		diagramId: "restoration_repair_bodywork_part",
 		diagramOrder: 4,
-		sensorsRelated: ["Electric Sander", "Polisher", "Manual Sander"]
+		sensorsRelated: ["Hammer"]
 	},
 	{
 		activityId: "Activity_1qneygj",
@@ -54,48 +54,48 @@ db.ActivityMaps.insertMany([
 		activityId: "Activity_0ei3vi0",
 		diagramId: "restoration_repair_bodywork_coating",
 		diagramOrder: 1,
-		sensorsRelated: ["Electric Sander", "Polisher", "Manual Sander"]
+		sensorsRelated: ["Paint"]
 	},
 	{
 		activityId: "Activity_1o6fb62",
 		diagramId: "restoration_repair_bodywork_coating",
 		diagramOrder: 2,
-		sensorsRelated: ["Electric Sander", "Polisher", "Manual Sander"]
+		sensorsRelated: ["Paint"]
 	},
 	{
 		activityId: "Activity_023mhjq",
 		diagramId: "restoration_repair_paint_complete",
 		diagramOrder: 1,
-		sensorsRelated: ["Electric Sander", "Polisher"]
+		sensorsRelated: ["Polisher"]
 	},
 	{
 		activityId: "Activity_1ri966u",
 		diagramId: "restoration_repair_paint_complete",
 		diagramOrder: 2,
-		sensorsRelated: ["Electric Sander", "Polisher"]
+		sensorsRelated: ["Paint"]
 	},
 	{
 		activityId: "Activity_0zcjcdk",
 		diagramId: "restoration_repair_paint_part_glasurit",
 		diagramOrder: 1,
-		sensorsRelated: ["Electric Sander", "Polisher"]
+		sensorsRelated: ["Polisher"]
 	},
 	{
 		activityId: "Activity_0mbkfar",
 		diagramId: "restoration_repair_paint_part_glasurit",
 		diagramOrder: 2,
-		sensorsRelated: ["Electric Sander", "Polisher"]
+		sensorsRelated: ["Electric Sander", "Manual Sander"]
 	},
 	{
 		activityId: "Activity_0j2usb8",
 		diagramId: "restoration_repair_paint_part_glasurit",
 		diagramOrder: 3,
-		sensorsRelated: ["Electric Sander", "Polisher"]
+		sensorsRelated: ["Electric Sander", "Manual Sander"]
 	},
 	{
 		activityId: "Activity_1mntowc",
 		diagramId: "restoration_repair_paint_part_glasurit",
 		diagramOrder: 4,
-		sensorsRelated: ["Electric Sander", "Polisher"]
+		sensorsRelated: ["Polisher"]
 	}
 ]);
